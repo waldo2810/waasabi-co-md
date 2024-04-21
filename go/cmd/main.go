79285f74743da70e5dbe8f39f3069faf748cbf87
Waldo Sobrino <waldo.sobrino@republicnetwork.es>
2024-04-20 22:32:12 -0500

    feat(core): integrate in main.go

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"waasabico/config"
+	"waasabico/models"
+	"waasabico/routes"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// Load env
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Init Echo
 	e := echo.New()
-	e.GET("/posts", getPosts)
+
+	// Connect db.
+	config.InitDatabase()
+	defer config.GetDatabase().DB()
+
+	// Migrations with AutoMigrate
+	db := config.GetDatabase()
+	err := db.AutoMigrate(&models.Post{}, &models.User{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup routes
+	routes.SetupRoutes(e)
+
+	// Start server
 	e.Logger.Fatal(e.Start(":3001"))
 }
 
