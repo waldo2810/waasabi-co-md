c58406fa51e5d1f8489d397ea3048367ba6f4a33
Waldo Sobrino <waldo.sobrino@republicnetwork.es>
2024-04-21 22:59:09 -0500

    refactor: delete admin stuff

--- src/pages/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-export default function AdminPage() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/admin/posts");
-  }, []);
-}
