a6439eaa3b6e1e8ec86c6bfd07b54ba043cf9782
Waldo Sobrino <waldo2810@gmail.com>
2024-06-21 22:36:14 -0500

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,31 @@
 const Footer = () => {
-  return (
-    <footer className="mb-10 text-xs flex flex-col items-center justify-center gap-1">
-      <span>
-        Want to tell me anything (literally)? email me{" "}
-        <a href="mailto:hey@waasabi.co" target="_blank">
-          here
-        </a>
-        .
-      </span>
-      <span>
-        Read my (un)useful tweets{" "}
-        <a href="https://twitter.com/waasabi_co" target="_blank">
-          here
-        </a>
-        .
-      </span>
-      <span>
-        Check are.na{" "}
-        <a href="https://www.are.na/wasabi-co" target="_blank">
-          here
-        </a>
-        .
-      </span>
-    </footer>
-  );
+    return (
+        <footer className="mb-10 text-xs flex items-center justify-center gap-1">
+            <span>
+                <a href="mailto:hey@waasabi.co" target="_blank">
+                    Email
+                </a>
+            </span>
+            |
+            <span>
+                <a href="https://twitter.com/waasabi_co" target="_blank">
+                    Twitter
+                </a>
+            </span>
+            |
+            <span>
+                <a href="https://www.are.na/wasabi-co" target="_blank">
+                    Are.na
+                </a>
+            </span>
+            |
+            <span>
+                <a href="https://www.are.na/wasabi-co" target="_blank">
+                    Github
+                </a>
+            </span>
+        </footer>
+    );
 };
 
 export default Footer;
